fad9bbfdadefae4a035a4fc7223110293cebed99
and47 <and47@users.noreply.github.com>
2023-03-31 11:41:23 +0100

    Update rollcorr_incrmtupd_par.py
    added option for k-step, k>1

--- rollcorr_incrmtupd_par.py
+++ rollcorr_incrmtupd_par.py
@@ -35,3 +35,76 @@ def incremental_corr(data1, data2, window_size, rolling=True):
         corr[i] = cov[i] / np.sqrt(var1 * var2)
 
     return corr, cov
+
+
+
+
+from numba import njit
+import numpy as np
+
+@njit
+def rolling_corr(data1, data2, window_size, k=1):
+    """
+    Compute rolling correlation between two arrays using a sliding window.
+
+    Parameters
+    ----------
+    data1 : ndarray
+        First input data.
+    data2 : ndarray
+        Second input data.
+    window_size : int
+        Size of the sliding window.
+    k : int, optional
+        Number of steps to move the window at each iteration. Defaults to 1.
+
+    Returns
+    -------
+    corr : ndarray
+        Array of correlation coefficients, one for each window.
+
+    Raises
+    ------
+    ValueError
+        If data1 and data2 have different lengths.
+
+    """
+    if len(data1) != len(data2):
+        raise ValueError("Input arrays must have the same length")
+
+    n = len(data1)
+    corr = np.empty(n - window_size + 1, dtype=np.float64)
+
+    # Compute mean and variance of the first window
+    window1 = data1[:window_size]
+    window2 = data2[:window_size]
+    mean1 = np.mean(window1)
+    mean2 = np.mean(window2)
+    var1 = np.var(window1, ddof=1)
+    var2 = np.var(window2, ddof=1)
+    cov = np.cov(window1, window2, ddof=1)[0, 1]
+
+    # Compute correlation coefficient for first window
+    corr[0] = cov / np.sqrt(var1 * var2)
+
+    # Compute correlation coefficients for subsequent windows
+    for i in range(1, n - window_size + 1):
+        old1 = data1[i - 1]
+        new1 = data1[i + window_size - 1]
+        old2 = data2[i - 1]
+        new2 = data2[i + window_size - 1]
+
+        # Update mean and variance incrementally
+        mean1 += (new1 - old1) * k / window_size
+        mean2 += (new2 - old2) * k / window_size
+        var1 += ((new1 - mean1) ** 2 - (old1 - mean1) ** 2) * k / window_size
+        var2 += ((new2 - mean2) ** 2 - (old2 - mean2) ** 2) * k / window_size
+
+        # Update covariance incrementally
+        cov += ((new1 - old1) * (new2 - old2) * k / window_size -
+                (k / (window_size - k)) * (mean1 - old1) * (mean2 - old2))
+
+        # Compute correlation coefficient for current window
+        corr[i] = cov / np.sqrt(var1 * var2)
+
+    return corr
